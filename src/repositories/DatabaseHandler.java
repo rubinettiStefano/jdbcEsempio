642c375d04a9509fb660bb608e16b06a3936c903
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-21 11:53:40 +0200

    v1

--- src/repositories/DatabaseHandler.java
+++ src/repositories/DatabaseHandler.java
@@ -3,6 +3,7 @@ package repositories;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Person;
 
@@ -28,6 +29,48 @@ public class DatabaseHandler
         }
     }
 
+    public ArrayList<Person> selectAll()
+    {
+        try 
+        {
+            return pRepo.selectAll();
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+            System.exit(-1);
+            return null;
+        }  
+    }
+
+    public ArrayList<Person> select(String condition)
+    {
+        try 
+        {
+            return pRepo.select(condition);
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+            System.exit(-1);
+            return null;
+        }  
+    }
+
+    public Person select(int id)
+    {
+        try 
+        {
+            return pRepo.select(id);
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+            System.exit(-1);
+            return null;
+        }  
+    }
+
     public void insertPerson(Person p)
     {
         try 
@@ -40,5 +83,44 @@ public class DatabaseHandler
             System.exit(-1);
         }  
         
+    }
+
+    public void updatePerson(Person p)
+    {
+        try 
+        {
+            pRepo.update(p);
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+            System.exit(-1);
+        }  
+    }
+
+    public void deletePerson(Person p)
+    {
+        try 
+        {
+            pRepo.delete(p);
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+            System.exit(-1);
+        }  
+    }
+
+    public void deletePerson(int id)
+    {
+        try 
+        {
+            pRepo.delete(id);
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+            System.exit(-1);
+        }  
     }
 }
--- src/repositories/PersonRepository.java
+++ src/repositories/PersonRepository.java
@@ -2,7 +2,9 @@ package repositories;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Person;
 
@@ -16,6 +18,39 @@ public class PersonRepository
     }
 
 
+    public ArrayList<Person> selectAll() throws SQLException
+    {
+        return select("1=1");
+    }
+
+    public ArrayList<Person> select(String condition) throws SQLException
+    {
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM person WHERE ?");
+        ps.setString(1, condition);
+
+        ArrayList<Person> res = new ArrayList<>();
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next())
+            res.add(convertToPerson(rs));
+
+        return res;
+    }
+
+    public Person select(int id) throws SQLException
+    {
+        return select("id="+id).get(0);
+    }
+
+    private Person convertToPerson(ResultSet rs) throws SQLException
+    {
+        Person p = new Person();
+        p.setId(rs.getInt("id"));
+        p.setName(rs.getString("name"));
+        p.setSurname(rs.getString("surname"));
+
+        return p;
+    }
     //Eccezioni Checked e Unchecked
     //Unchecked sono quelle che abbiamo visto fino ad ora
     //Le Checked vi obbligano a dichiarare una gestione ESPLICITA dell'eccezione
@@ -32,5 +67,37 @@ public class PersonRepository
         pStatement.execute();
         pStatement.close();
     }
+
+    public void update(Person p) throws SQLException
+    {
+        String query = "UPDATE person SET name=?,surname=? WHERE id=?";
+        
+        PreparedStatement pStatement = con.prepareStatement(query);
+        pStatement.setString(1, p.getName());
+        pStatement.setString(2, p.getSurname());
+        pStatement.setInt(3, p.getId());
+
+
+        pStatement.execute();
+        pStatement.close();
+    }
+
+    public void delete(int id) throws SQLException
+    {
+        String query = "DELETE FROM person WHERE id=?";
+        
+        PreparedStatement pStatement = con.prepareStatement(query);
+        pStatement.setInt(1,id);
+
+
+        pStatement.execute();
+        pStatement.close();
+    }
+
+    public void delete(Person p) throws SQLException
+    {
+        delete(p.getId());
+    }
+    
     
 }
