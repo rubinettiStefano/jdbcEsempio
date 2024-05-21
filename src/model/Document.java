d1faaeb597ebc94836580111fc92fb6070db98f1
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-21 11:56:28 +0200

    v

---
+++ src/model/Document.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * In futuro (tra 30 min), sarà COLLEGATA come figlio a Person
+ * Per ora vi chiedo di farla come Person, di fare la sua Repository e aggiungere i metodi nell'dbhandler
+ * id
+ * type
+ * releaseDate
+ * expirationDate
+ */
+public class Document 
+{
+
+}
